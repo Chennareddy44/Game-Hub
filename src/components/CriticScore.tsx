a705e3b97033d4524b7b6fdefc44fbe857b920d4
chennareddy44 <20btrcs023@jainuniversity.ac.in>
2023-09-29 19:16:41 +0530

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,21 @@
+import { Badge } from "@chakra-ui/react";
+
+interface props {
+  score: number;
+}
+
+const CriticScore = ({ score }: props) => {
+  let color = score > 80 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
