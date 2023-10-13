4ea9c1eb0f92039ca46a640cb1488d80ecc67c80
chennareddy44 <20btrcs023@jainuniversity.ac.in>
2023-10-13 11:04:17 +0530

    update api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "6c930516101543598963918785f72bb5",
+    key: "b874e319fab947169cb54555e33f61f7",
+    // key: "6c930516101543598963918785f72bb5",
   },
 });
